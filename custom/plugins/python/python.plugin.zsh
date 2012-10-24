860d4b49f803953223f3549c935ff92fec7e54ab
Nikos Nikoleris <nikos.nikoleris@gmail.com>
2012-10-24 15:07:03 +0200

    add custom python plugin

---
+++ custom/plugins/python/python.plugin.zsh
@@ -0,0 +1,9 @@
+# Find python file
+alias pyfind='find . -name "*.py"'
+
+# Grep among .py files
+alias pygrep='grep --include="*.py"'
+
+# Startup for interactive python
+export PYTHONSTARTUP=${HOME}/.pythonrc
+
